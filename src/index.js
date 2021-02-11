7d3d7217c819ac2945d03c58f8e9e848bea0f5b4
Gabriel A <tfmdev91@gmail.com>
2021-02-11 00:18:24 -0300

    Pb

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+
+server = app.listen(3000, console.log('open'));
+
+var clients = {}
+
+const chat_history = new Array();
+
+const io = require('socket.io')(server);
+
+io.on('connection', client => {
+  client.on("join", function(name){
+    console.log("Joined: " + name);
+    clients[client.id] = name;
+    client.emit("update", "Você está conectado.");
+    client.emit('history', chat_history);
+    client.username = name;
+    client.broadcast.emit("update", name + " has joined the server.");
+  });
+
+  client.on('change_username', username => {
+    client.username = username;
+    console.log(username) 
+  });
+
+  client.on('new_message', data => {
+    console.log(data)
+    chat_history.push({message: data.msg, username: client.username, date: data.data, hour: data.hora});
+    client.broadcast.emit('user_msg', {message: data.msg, username: client.username, date: data.data, hour: data.hora})
+    client.emit('user_msg', {message: data.msg, username: client.username, date: data.data, hour: data.hora})
+  });
+
+  client.on("disconnect", function(){
+    console.log("Disconnect");
+    io.emit("update", clients[client.id] + " has left the server.");
+    delete clients[client.id];
+  });
+});
---
+++ src/public/assets/js/chat.js
@@ -0,0 +1,129 @@
+const socket = io.connect('http://192.168.0.126:3000');
+
+if(localStorage.getItem('name') === null) {
+  var person = prompt("Como quer ser chamado(a) ?", "");
+  if (person == null || person == "") {
+    person = prompt("Como quer ser chamado(a) ?", "");
+  } else {
+    localStorage.setItem('name', person);
+    socket.emit('join', person);
+  };
+}else{
+  socket.emit('join', localStorage.getItem('name'))
+}
+
+socket.on('getName', () => {
+  socket.emit('join', localStorage.getItem('name'))
+})
+
+const act = {
+  hora: () => {
+    let d = new Date();
+    let n = d.getHours();
+    let s = d.getMinutes();
+    let hours = n + ':' + (s <= 10 ? '0' + s : s)
+    return hours
+  },
+  data: () => {
+    let d = new Date();
+    let dia = d.getDate();
+    let m = d.getMonth();
+    let mes = new Array(
+      'janeiro', 'fevereiro', 'março', 'abril', 'maio',
+      'junho', 'julho', 'agosto', 'setembro', 'outubro',
+      'novembro', 'dezembro'
+    )
+    return (dia + ' de ' + mes[m])
+  }
+  
+}
+
+socket.on('history', data => {
+  msgHistory(data)
+  console.log(data)
+})
+
+$('#msg').keypress(e => {
+  if(e.keyCode == 13){
+    e.preventDefault()
+    sendMessage()
+  }
+})
+
+$('#send_msg').on('click', () => {
+  sendMessage()
+})
+
+function sendMessage() {
+  let msg = $('#msg').val(); $('#msg').val('')
+  let hora = act.hora();
+  let send = {msg, hora, data: act.data()}
+  console.log($('#msg').val(), act.hora(), act.data())
+  socket.emit('new_message', send)
+}
+
+socket.on('user_msg', data => {
+  if(!data) return
+  console.log('Mensagem:', data)
+  if(data.username == localStorage.getItem('name')){
+    $('#appCapsule').append(`
+      <div class="message-item user">
+      <div class="content">
+          <div class="bubble">
+              ${data.message}
+          </div>
+          <div class="footer">${data.hour}</div>
+      </div>
+      </div>
+    `) 
+  }else{
+    $('#appCapsule').append(`
+      <div class="message-item">
+      <img src="assets/img/default-user-image.png" alt="avatar" class="avatar">
+      <div class="content">
+          <div class="title">${data.username}</div>
+          <div class="bubble">
+              ${data.message}
+          </div>
+          <div class="footer">${data.hour}</div>
+      </div>
+      </div>
+    `) 
+  }
+});
+
+function msgHistory(data) {
+  if(!data) return
+  $('#appCapsule').empty()
+  $('#appCapsule').append(`
+  <div class="message-divider">
+    ${act.data()}
+  </div> `);
+  data.map(item => {
+    if(item.username == localStorage.getItem('name')){
+      $('#appCapsule').append(`
+      <div class="message-item user">
+      <div class="content">
+          <div class="bubble">
+              ${item.message}
+          </div>
+          <div class="footer">${item.hour}</div>
+      </div>
+      </div>
+    `) 
+    }else{
+      $('#appCapsule').append(`
+      <div class="message-item">
+      <img src="assets/img/default-user-image.png" alt="avatar" class="avatar">
+      <div class="content">
+          <div class="title">${item.username}</div>
+          <div class="bubble">
+              ${item.message}
+          </div>
+          <div class="footer">${item.hour}</div>
+      </div>
+      </div>
+    `) 
+    }
+  })
+}
